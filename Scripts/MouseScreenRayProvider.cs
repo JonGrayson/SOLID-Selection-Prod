714a3269bb85d5a95085d2f087d6a77170d1f8a1
JonGrayson <49501559+JonGrayson@users.noreply.github.com>
2020-11-16 21:13:13 -0700

    Add files via upload

---
+++ Scripts/MouseScreenRayProvider.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseScreenRayProvider : MonoBehaviour, IRayProvider
+{
+    public Ray CreateRay()
+    {
+        return Camera.main.ViewportPointToRay(new Vector3(.5f, .5f, .5f));
+    }
+}
---
+++ Scripts/SizeSelection.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class SizeSelection : MonoBehaviour, ISelectionResponse
+{
+    [SerializeField] public Material selectedMaterial;
+    [SerializeField] public Material defaultMaterial;
+
+    public Transform originalSize;
+    public void OnSelect(Transform selection)
+    {
+        originalSize = selection.GetComponent<Transform>();
+        var size = selection.GetComponent<Transform>();
+        size.localScale *= .5f;
+        var selectionRenderer = selection.GetComponent<Renderer>();
+        if (selectionRenderer != null)
+        {
+            selectionRenderer.material = this.selectedMaterial;
+        }
+
+    }
+
+    public void OnDeselect(Transform selection)
+    {
+        var size = selection.GetComponent<Transform>();
+        size.localScale *= 2f;
+
+        var selectionRenderer = selection.GetComponent<Renderer>();
+        if (selectionRenderer != null)
+        {
+            selectionRenderer.material = this.defaultMaterial;
+        }
+    }
+}
